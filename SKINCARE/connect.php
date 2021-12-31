88ef90c228a0c1ca847b76614fb32beb1f1cafe3
Diana-nyamai <dnyamai.dn@gmail.com>
2021-12-31 12:44:06 +0300

    update

--- SKINCARE/connect.php
+++ SKINCARE/connect.php
@@ -13,12 +13,7 @@ if(mysqli_connect_error()){
     die('connection failed(' .mysqli_connect_error().')' . mysqli_connect_error());
 }
 else{
-<<<<<<< HEAD
     $stmt = $conn->prepare('insert into tbl_users(user_first_name, user_last_name, user_phone_no, user_email,user_gender, user_role, user_password) 
-=======
-    $stmt = $conn->prepare('insert into tbl_users(user_first
-    _name, user_last_name, user_phone_no, user_email, user_role, user_password, user_gender) 
->>>>>>> 4731ae6a817de11fec2d78fa70829a245c5d2777
     values(?,?,?,?,?,?,?)');
     $stmt->bind_param('ssissss', $fname, $lname,$phone,$email, $gender, $user, $password);
     $stmt->execute();
--- SKINCARE/post.php
+++
@@ -1,6 +0,0 @@
-<?php
- $fname = $_POST['fname'];
- $lname = $_POST['lname'];
-
- echo 'my first name is $fname and my last name is $lname';
-?>
