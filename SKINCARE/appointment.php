f09cf612e8fe1c06055bb1755475208381d0a203
Diana-nyamai <dnyamai.dn@gmail.com>
2021-12-31 15:51:05 +0300

    update

---
+++ SKINCARE/appointment.php
@@ -0,0 +1,23 @@
+<?php
+  $fname = $_POST['fname'];
+  $lname = $_POST['lname'];
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+  $date = $_POST['date'];
+  $time = $_POST['time'];
+ 
+//   database connection
+$conn = new mysqli('localhost', 'root', '', 'skincare');
+if(mysqli_connect_error()){
+    die('connection failed(' .mysqli_connect_error(). ')' . mysqli_Connect_error());
+}
+else{
+    $stmt = $conn->prepare('insert into tbl_appointment(first_name, last_name, phone_no, email, date, time)
+    values(?,?,?,?,?,?)');
+    $stmt->bind_param('ssisss', $fname, $lname,$phone, $email, $date,$time );
+    $stmt->execute();
+    echo 'appointment booked...';
+    $stmt->close();
+    $conn->close();
+}
+?>
