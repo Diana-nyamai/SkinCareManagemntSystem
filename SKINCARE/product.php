c17ce726de4c37395788944008615c4ec6da06d8
Diana-nyamai <dnyamai.dn@gmail.com>
2022-01-10 15:57:52 +0300

    update

--- SKINCARE/product.php
+++ SKINCARE/product.php
@@ -10,12 +10,12 @@ $pdescription = $_POST['pdescription'];
 $pprice = $_POST['pprice'];
 
 // database connetion
-$conn = new mysqli('localhost', 'root', '', 'skincare');
+$conn = new mysqli('localhost', 'ndinda', 'dnyamai.dn', 'skincare');
 if(mysqli_connect_error()){
     die ('connection failed (' . mysqli_connect_error() . ')' . mysqli_connect_error());
 }
 else{
-    $stmt = $conn->prepare('insert into tbl_product(sname, owner, phone_no, email, pname, ptype,brand,description,price)
+    $stmt = $conn->prepare('insert into tbl_product(sname, sowner, phone_no, email, pname, ptype,brand,pdescription,price)
     values(?,?,?,?,?,?,?,?,?)');
     $stmt->bind_param('ssisssssi', $sname, $sowner, $sphone, $semail, $pname, $ptype, $pbrand,$pdescription,$pprice);
     $stmt->execute();
--- SKINCARE/productReport.php
+++ SKINCARE/productReport.php
@@ -5,13 +5,13 @@
 <!-- <a href="#" onclick="window.print();">print</a>  -->
 
 <?php
-$connection = new mysqli('localhost', 'root', '', 'skincare');
+$connection = new mysqli('localhost', 'ndinda', 'dnyamai.dn', 'skincare');
 $productq = mysqli_query($connection, 'SELECT * FROM tbl_product');
 $count = mysqli_num_rows($productq);
 echo  $count, ' records found <br/>';
 
 // main table
-echo "<table border= '1' align= 'center'>";
+echo "<table border= '1' align= 'center' background: '#000'; color: 'white';>";
 
 // header of the table
 echo "<tr>";
@@ -32,13 +32,13 @@ while($row = mysqli_fetch_array($productq)){
     echo "<tr>";
     echo "<td>{$row['shop_id']}</td>";
     echo "<td>{$row['sname']}</td>";
-    echo "<td>{$row['owner']}</td>";
+    echo "<td>{$row['sowner']}</td>";
     echo "<td>{$row['phone_no']}</td>";
     echo "<td>{$row['email']}</td>";
     echo "<td>{$row['pname']}</td>";
     echo "<td>{$row['ptype']}</td>";
     echo "<td>{$row['brand']}</td>";
-    echo "<td>{$row['description']}</td>";
+    echo "<td>{$row['pdescription']}</td>";
     echo "<td>{$row['price']}</td>";
     echo "</tr>";
 }
