5e4604f35cf903ef72df3631ae91b67fb09fd6fd
Diana-nyamai <dnyamai.dn@gmail.com>
2022-01-17 13:37:08 +0300

    update

---
+++ SKINCARE/login1.php
@@ -0,0 +1,31 @@
+<?php
+session_start(); 
+//  database connection
+$conn = new mysqli('localhost', 'ndinda', 'dnyamai.dn', 'skincare');
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $password = $_POST['lpassword'];
+
+    $s = "select * from tbl_users where first_name ='".$fname."'&& password = '".$password."'";
+    $result = mysqli_query($conn, $s);
+    $row = mysqli_fetch_array($result);
+
+    if($row['user_type'] == 'customer'){
+        $_SESSION['username'] = $fname;
+        header('location:home.php');
+    }
+    elseif($row['user_type'] == 'dermatologist'){
+        header('location:admin.html');
+    }
+    elseif($row['user_type'] == 'admin'){
+        header('location:admin.html');
+    }
+    else{
+        echo "wrong credentials";
+    }
+}
+ 
+
+?>
--- SKINCARE/signup.php
+++ SKINCARE/signup.php
@@ -13,7 +13,7 @@ if(mysqli_connect_error()){
     die('connection failed(' .mysqli_connect_error().')' . mysqli_connect_error());
 }
 else{
-    $stmt = $conn->prepare('insert into tbl_users(first_name, last_name, phone_number, email, user_type, gender,password) 
+    $stmt = $conn->prepare('insert into tbl_users(first_name, last_name, phone_number, email,gender, user_type, password) 
     values(?,?,?,?,?,?,?)');
     $stmt->bind_param('ssissss', $fname, $lname,$phone,$email, $gender, $user, $password);
     $stmt->execute();
